0e9b4a4801923c2dd5ebadec6e7f966dbb27574d
yuma11 <11yuma20@gmail.com>
2023-12-11 13:08:59 +0900

    first commit

---
+++ inc/update.php
@@ -0,0 +1,89 @@
+<?php
+namespace OPR\AfiLT;
+
+
+function get_update_data() {
+
+	// GitHub APIを使って、Releaseの最新バージョン情報を取得する
+	$response = wp_remote_get( 'https://raw.githubusercontent.com/ddryo/afilink-extractor/main/version.json' );
+
+	// レスポンスエラー
+	if( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+		return null;
+	}
+
+	$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+	
+	return array(
+		'version' => $response_body['version'] ?? null,  // 最新のバージョン
+		'package' => $response_body['package'] ?? null,  // zipファイルパッケージのURL
+	);
+}
+
+
+function get_readme() {
+
+	$cache = get_transient('wp_afilt_readme_cache');
+	if ($cache) return $cache;
+	$response = wp_remote_get( 'https://raw.githubusercontent.com/ddryo/afilink-extractor/main/README.md' );
+
+	// レスポンスエラー
+	if( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+		return null;
+	}
+	$response_body = wp_remote_retrieve_body( $response );
+
+	set_transient( 'wp_afilt_readme_cache', $response_body, 24 * HOUR_IN_SECONDS );
+	return $response_body;
+}
+
+
+
+/**
+ * アップデートチェック
+ */
+add_filter( 'update_plugins_ls-afilink-extractor', function( $update, $plugin_data ) {
+	if ( ! current_user_can( 'manage_options' ) ) return $update;
+	$update_data = get_update_data();
+	if ( ! $update_data ) return $update;
+
+	return array(
+		'slug'        => 'wp-affiliate-link-tracker', // plugins_api に必要
+		'version'     => $plugin_data['Version'], // 現在のバージョン
+		'new_version' => $update_data['version'] ?? '', // 最新のバージョン
+		'package'     => $update_data['package'] ?? '', // zipファイルパッケージのURL
+	);
+}, 10, 2 );
+
+
+// 詳細表示のポップアップ情報をセット
+add_filter( 'plugins_api', function( $res, $action, $args ) {
+
+	if ( 'plugin_information' !== $action ) return $res;
+	if ( $args->slug !== 'wp-affiliate-link-tracker' ) return $res;
+
+	// アップデート用のデータを取得
+	$update_data = get_update_data();
+	if ( ! $update_data ) return $res;
+
+	$readme = get_readme();
+
+	// 改行コードをbrタグに変換
+	$readme = nl2br( $readme );
+
+	
+	$readme = preg_replace( '/<img([^>]+)>/', '<ul><li><img$1></li></ul>', $readme );
+
+	return (object) array(
+		'name' => 'Afilink Extractor',
+		'slug' => 'wp-affiliate-link-tracker',
+		'path' => 'afilink-extractor/index.php',
+		'version' => $update_data['version'] ?? '',
+		'download_link' =>  $update_data['package'] ?? '',
+		'sections' => array(
+			// 'description' => $readme,
+			'screenshots' => $readme, // img がはみ出さないようにするために screenshots タブで表示。
+			
+		),
+	);
+}, 20, 3 );
---
+++ index.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Plugin Name: WP Affiliate Link Tracker
+ * Description: ASP広告掲載URL抽出用プラグイン
+ * Version: 0.0.1
+ * Plugin URI: 
+ * Author: 
+ * Author URI: 
+ * License: GPL2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-affiliate-link-tracker
+ * Domain Path: /languages
+ * Update URI: wp-affiliate-link-tracker
+ */
+
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/inc/update.php';
+
+/**
+ * menu 追加
+ */
+add_action('admin_menu', function () {
+	add_submenu_page(
+		'tools.php', // 親メニューのスラッグ
+		'広告URL抽出', // head内のtitleタグに出力される文字列
+		'広告URL抽出', // 管理画面左側のメニュー欄に表示される文字列
+		'manage_options', // メニュー欄に表示するためのユーザー権限
+		'wp-affiliate-link-tracker', // このメニューを参照するスラッグ名
+		'wp_afilt__dashboard', // メニューに紐づく画面を描画するcallback関数
+		'', // アイコン see: https://developer.wordpress.org/resource/dashicons/#awards
+		99, // 表示位置のオフセット
+	);
+});
+
+function wp_afilt__checknonce()
+{
+	return wp_verify_nonce($_POST['wp-afilt_nonce'], 'wp-afilt');
+}
+
+function wp_afilt__dashboard()
+{
+	require_once __DIR__ . '/inc/get_all_links.php';
+	$cache = false;
+	if (isset($_POST['wp-afilt-delete-cache']) && wp_afilt__checknonce()) {
+		delete_transient('wp_afilt_data_cache');
+	}
+
+?>
+	<style>
+		.wrap {
+			padding-block: 2rem
+		}
+
+		.toolBtns {
+			margin-block: 1.5em;
+			display: flex;
+			gap: 1em;
+		}
+
+		.confArea {
+			padding: 1.5em;
+			background: #fff;
+			border: solid 1px #ddd;
+		}
+
+		.confTable :is(td, th) {
+			padding: 2px 6px;
+			border: solid 1px gray
+		}
+	</style>
+<?php
+	echo '<div class="wrap">';
+
+	$cache = get_transient('wp_afilt_data_cache');
+	$has_cache = !!$cache;
+	$ext_label = $has_cache ? 'リンクを再抽出' : 'リンクを抽出';
+	$table_caption = $has_cache ? 'キャッシュ済みデータ (24時間保存されます。)' : '新規抽出データ';
+
+	echo '<form method="post">';
+	wp_nonce_field('wp-afilt', 'wp-afilt_nonce');
+
+	echo '<div class="toolBtns">';
+	echo '<button type="submit" name="wp-afilt-check" class="button button-primary">' . $ext_label . '</button>';
+	echo '<button type="submit" name="wp-afilt-download" class="button">CSVダウンロード</button>';
+	echo '</div>';
+
+
+	if ($has_cache) {
+		$link_list = $cache;
+	}
+
+	if (isset($_POST['wp-afilt-check']) && wp_afilt__checknonce()) {
+		delete_transient('wp_afilt_data_cache');
+		try {
+			$link_list = \OPR\AfiLT\get_a8links();
+			$table_caption = '新規抽出データ';
+			if (empty($link_list)) {
+				echo '<p>リンクは見つかりませんでした。</p>';
+			}
+		} catch (\Throwable $th) {
+			echo '<p>エラーが発生しました</p>';
+			echo '<p>' . $th->getMessage() . '</p>';
+		}
+	}
+
+
+	if (!empty($link_list)) {
+		echo '<div class="confArea">';
+
+
+		echo '<table class="confTable">';
+		if ($table_caption) {
+			echo '<caption>' . $table_caption . '</caption>';
+		}
+		echo '<tr><th>記事url</th><th>プログラムID</th><th>使用リンク</th></tr>';
+		foreach ($link_list as $link) {
+			echo '<tr>';
+			foreach ($link as $value) {
+				echo '<td>' . $value . '</td>';
+			}
+			echo '</tr>';
+		}
+		echo '</table>';
+		echo '</div>';
+
+
+		if ($has_cache) {
+			echo '<div class="toolBtns">';
+			echo '<button type="submit" name="wp-afilt-delete-cache" class="button">キャッシュ削除</button>';
+			echo '</div>';
+		}
+		echo '</form>';
+	}
+
+	echo '</div>';
+}
+
+
+add_action('admin_init', function () {
+	if (!isset($_POST['wp-afilt-download'])) return;
+	if (!wp_afilt__checknonce()) return;
+
+	try {
+		require_once __DIR__ . '/inc/get_all_links.php';
+		$link_list = \OPR\AfiLT\get_a8links();
+	} catch (\Throwable $th) {
+		echo '<p>エラーが発生しました</p>';
+		echo '<p>' . $th->getMessage() . '</p>';
+	}
+
+	require_once __DIR__ . '/inc/array_to_csv.php';
+	$csv = \OPR\AfiLT\array_to_csv($link_list, ['記事url', 'プログラムID', '使用リンク']);
+
+	// 出力
+	$filename = 'a8_links_' . wp_date('Ymd') . '.csv';
+	header('Content-Type: application/csv');
+	header('Content-Disposition: attachment; filename="' . $filename . '";');
+	echo $csv;
+	exit;
+});
